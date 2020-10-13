3105f7925e5231f08b6ce9d220e1d8db4a3f1adf
thibautgravey <thibaut.gravey@insa-lyon.fr>
2020-10-13 08:02:04 +0200

    indent lines and sysout for Multi Threaded Server

--- Code-Socket/src/stream/ClientThread.java
+++ Code-Socket/src/stream/ClientThread.java
@@ -11,31 +11,32 @@ import java.io.*;
 import java.net.*;
 
 public class ClientThread
-	extends Thread {
-	
+		extends Thread {
+
 	private Socket clientSocket;
-	
+
 	ClientThread(Socket s) {
 		this.clientSocket = s;
 	}
 
- 	/**
-  	* receives a request from client then sends an echo to the client
-  	* @param clientSocket the client socket
-  	**/
+	/**
+	 * receives a request from client then sends an echo to the client
+	 * @param clientSocket the client socket
+	 **/
 	public void run() {
-    	  try {
-    		BufferedReader socIn = null;
-    		socIn = new BufferedReader(
-    			new InputStreamReader(clientSocket.getInputStream()));    
-    		PrintStream socOut = new PrintStream(clientSocket.getOutputStream());
-    		while (true) {
-    		  String line = socIn.readLine();
-    		  socOut.println(line);
-    		}
-    	} catch (Exception e) {
-        	System.err.println("Error in EchoServer:" + e); 
-        }
-       }
-  
-  }
+		try {
+			BufferedReader socIn = null;
+			socIn = new BufferedReader(
+					new InputStreamReader(clientSocket.getInputStream()));
+			PrintStream socOut = new PrintStream(clientSocket.getOutputStream());
+			while (true) {
+				String line = socIn.readLine();
+				socOut.println(line);
+				System.out.println(line);
+			}
+		} catch (Exception e) {
+			System.err.println("Error in EchoServer:" + e);
+		}
+	}
+
+}
--- Code-Socket/src/stream/EchoServerMultiThreaded.java
+++ Code-Socket/src/stream/EchoServerMultiThreaded.java
@@ -11,31 +11,31 @@ import java.io.*;
 import java.net.*;
 
 public class EchoServerMultiThreaded  {
-  
- 	/**
-  	* main method
-	* @param EchoServer port
-  	* 
-  	**/
-       public static void main(String args[]){ 
-        ServerSocket listenSocket;
-        
-  	if (args.length != 1) {
-          System.out.println("Usage: java EchoServer <EchoServer port>");
-          System.exit(1);
-  	}
-	try {
-		listenSocket = new ServerSocket(Integer.parseInt(args[0])); //port
-		System.out.println("Server ready..."); 
-		while (true) {
-			Socket clientSocket = listenSocket.accept();
-			System.out.println("Connexion from:" + clientSocket.getInetAddress());
-			ClientThread ct = new ClientThread(clientSocket);
-			ct.start();
+
+	/**
+	 * main method
+	 * @param EchoServer port
+	 *
+	 **/
+	public static void main(String args[]){
+		ServerSocket listenSocket;
+
+		if (args.length != 1) {
+			System.out.println("Usage: java EchoServer <EchoServer port>");
+			System.exit(1);
 		}
-        } catch (Exception e) {
-            System.err.println("Error in EchoServer:" + e);
-        }
-      }
-  }
+		try {
+			listenSocket = new ServerSocket(Integer.parseInt(args[0])); //port
+			System.out.println("Server ready...");
+			while (true) {
+				Socket clientSocket = listenSocket.accept();
+				System.out.println("Connexion from:" + clientSocket.getInetAddress());
+				ClientThread ct = new ClientThread(clientSocket);
+				ct.start();
+			}
+		} catch (Exception e) {
+			System.err.println("Error in EchoServer:" + e);
+		}
+	}
+}
 
